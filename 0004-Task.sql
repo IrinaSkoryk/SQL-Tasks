19cf66775ca924a0f933129afa7b9f9e8b8ea506
IrinaSkoryk <irinaskoryk36@gmail.com>
2025-02-02 21:53:41 +0100

    Create 0004-Task.sql

---
+++ 0004-Task.sql
@@ -0,0 +1,29 @@
+-- #Easy #Comparison
+-- Show the employee_id, order_id, customer_id, required_date, shipped_date from all orders shipped later than the required date
+
+CREATE TABLE orders (
+  order_id INT PRIMARY KEY
+  , FOREIGN KEY (customer_id TEXT) REFERENCES customers (customer_id)
+  , FOREIGN KEY (employee_id INT) REFERENCES employees (empoyee_id)
+  , order_date DATE
+  , required_date DATE
+  , shipped_date DATE
+  , FOREIGN KEY (ship_via INT) REFERENCES shippers (shipper_id)
+  , freight DECIMAL
+  , ship_name TEXT
+  , ship_address TEXT
+  , ship_city TEXT
+  , ship_region TEXT
+  , ship_postal_code TEXT
+  , ship_country TEXT
+  )
+
+Select 
+  employee_id
+  , order_id
+  , customer_id
+  , required_date
+  , shipped_date
+FROM orders
+WHERE shipped_date > required_date;
+
