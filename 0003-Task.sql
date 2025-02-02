15831d8894d497339339a70c2470211969440670
IrinaSkoryk <irinaskoryk36@gmail.com>
2025-02-02 21:59:11 +0100

    Update 0003-Task.sql

--- 0003-Task.sql
+++ 0003-Task.sql
@@ -2,13 +2,13 @@
 -- Show order_date, shipped_date, customer_id, freight of all orders placed on 2018 Feb 26
 
 CREATE TABLE orders (
-  order_id INT PRIMARY KEY
-  , FOREIGN KEY (customer_id TEXT) REFERENCES customers (customer_id)
-  , FOREIGN KEY (employee_id INT) REFERENCES employees (empoyee_id)
+	order_id INT PRIMARY KEY
+	, FOREIGN KEY (customer_id TEXT) REFERENCES customers (customer_id)
+	, FOREIGN KEY (employee_id INT) REFERENCES employees (empoyee_id)
 	, order_date DATE
 	, required_date DATE
 	, shipped_date DATE
-  , FOREIGN KEY (ship_via INT) REFERENCES shippers (shipper_id)
+	, FOREIGN KEY (ship_via INT) REFERENCES shippers (shipper_id)
 	, freight DECIMAL
 	, ship_name TEXT
 	, ship_address TEXT
