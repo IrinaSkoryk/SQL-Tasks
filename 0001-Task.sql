b1cc22c193515d36ca4298f3ff68e824cae225c8
IrinaSkoryk <irinaskoryk36@gmail.com>
2025-02-02 20:00:55 +0100

    Create 0001-Task.sql

---
+++ 0001-Task.sql
@@ -0,0 +1,12 @@
+-- #Easy #Sort #ORDER_BY
+-- Show the category_name and description from the categories table sorted by category_name.
+
+CREATE TABLE categories (
+  category_id INT Primary key
+  , category_name TEXT
+  , description TEXT)
+SELECT 
+  category_name
+  , description
+FROM categories
+ORDER BY 1;
