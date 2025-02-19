b1c60a9903a8b2bbecb80d73f813999a0d3b3c86
IrinaSkoryk <irinaskoryk36@gmail.com>
2025-02-19 20:53:23 +0100

    Create 0019-Task.sql

---
+++ 0019-Task.sql
@@ -0,0 +1,30 @@
+-- #Easy #BETWEEN #WHERE #Comparison
+-- Show first name and last name of patients that weight within the range of 100 to 120 (inclusive)
+
+CREATE TABLE patients (  
+  patient_id INT PRIMARY KEY
+  first_name TEXT  
+  last_name TEXT  
+  gender CHAR
+  birth_date   DATE  
+  city   TEXT  
+  FOREIGN KEY (province_id) REFERENCES province_names (province_id)
+  allergies TEXT  
+  height INT  
+  weight INT
+);
+
+SELECT  
+  first_name  
+  , last_name
+FROM patients
+WHERE weight BETWEEN 100 AND 120;
+
+Another solution
+
+SELECT  
+  first_name  
+  , last_name
+FROM patients
+WHERE weight >=100  
+    AND weight <=120;
