55a71d5bfad45c7017647e3ea65e717f3f4e16c9
IrinaSkoryk <irinaskoryk36@gmail.com>
2025-02-19 22:42:48 +0100

    Create 0023-Task.sql

---
+++ 0023-Task.sql
@@ -0,0 +1,37 @@
+-- #Easy #Date #COUNT #calculation #BETWEEN #aggregation #aggregate_function #YEAR
+-- Show how many patients have a birth_date with 2010 as the birth year.
+
+CREATE TABLE patients (  
+  patient_id INT PRIMARY KEY
+  first_name TEXT  
+  last_name TEXT  
+  gender CHAR
+  birth_date   DATE  
+  city   TEXT  
+  FOREIGN KEY (province_id) REFERENCES province_names (province_id)
+  allergies TEXT  
+  height INT  
+  weight INT
+);
+
+SELECT  
+  COUNT(*) AS total_patients
+FROM patients
+WHERE YEAR(birth_date) = 2010;
+
+--Another Solution
+
+SELECT  
+  COUNT(first_name) AS total_patients
+FROM patients
+WHERE  
+  birth_date >= '2010-01-01'  
+  AND birth_date <= '2010-12-31';
+
+--Another Solution
+
+SELECT  
+  COUNT(first_name) AS total_patients
+FROM patients
+WHERE  
+  birth_date BETWEEN '2010-01-01' AND birth_date '2010-12-31';
