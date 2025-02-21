212f4e8a125e508eb9d1c0f9ce4b43c1c1a4bfb9
IrinaSkoryk <irinaskoryk36@gmail.com>
2025-02-21 22:56:33 +0100

    Update 0003-Task.sql

--- 0003-Task.sql
+++ 0003-Task.sql
@@ -1,22 +1,25 @@
 -- #Easy #Date 
 -- Show order_date, shipped_date, customer_id, freight of all orders placed on 2018 Feb 26
 
-CREATE TABLE orders (  
-	order_id INT PRIMARY KEY
-	, FOREIGN KEY (customer_id TEXT) REFERENCES customers (customer_id)
-	, FOREIGN KEY (employee_id INT) REFERENCES employees (empoyee_id)
-	, order_date DATE
-	, required_date DATE
-	, shipped_date DATE
-	, FOREIGN KEY (ship_via INT) REFERENCES shippers (shipper_id)
-	, freight DECIMAL
-	, ship_name TEXT
-	, ship_address TEXT
-	, ship_city TEXT
-	, ship_region TEXT
-	, ship_postal_code TEXT
-	, ship_country TEXT
-  );
+CREATE TABLE orders (
+  order_id INT PRIMARY KEY
+  , customer_id INT  
+  , employee_id INT
+  , order_date DATE
+  , required_date DATE
+  , shipped_date DATE
+  , ship_via INT  
+  , freight DECIMAL
+  , ship_name TEXT
+  , ship_address TEXT
+  , ship_city TEXT
+  , ship_region TEXT
+  , ship_postal_code TEXT
+  , ship_country TEXT
+  , FOREIGN KEY (ship_via) REFERENCES shippers (shipper_id)  
+  , FOREIGN KEY (customer_id) REFERENCES customers (customer_id)  
+  , FOREIGN KEY (employee_id) REFERENCES employees (employee_id)
+);
 
 SELECT 
   order_date
