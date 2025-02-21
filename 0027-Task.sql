e5ed35bdc8861bc82a066e7be66594f3e1788d15
IrinaSkoryk <irinaskoryk36@gmail.com>
2025-02-21 22:28:37 +0100

    Update 0027-Task.sql

--- 0027-Task.sql
+++ 0027-Task.sql
@@ -2,11 +2,14 @@
 -- Show all the columns from admissions where the patient was admitted and discharged on the same day.
 
 CREATE TABLE admissions (  
-  FOREIGN KEY (patient_id) REFERENCES patients (patient_id)  
-  admission_date DATE  
-  discharge_date   DATE  
-  diagnosis   TEXT
-  FOREIGN KEY (attending_doctor_id) REFERENCES doctors (doctor_id)
+  admission_id PRIMARY KEY  
+  , patient_id INT
+  , admission_date DATE  
+  , discharge_date DATE  
+  , diagnosis TEXT  
+  , attending_doctor_id INT  
+  , FOREIGN KEY (patient_id) REFERENCES patients (patient_id)  
+  , FOREIGN KEY (attending_doctor_id) REFERENCES doctors (doctor_id)  
 );
 
 SELECT *
