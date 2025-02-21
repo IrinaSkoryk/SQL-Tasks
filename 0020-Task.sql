b82708e2cdaffd3e7ac9d3d7fb2ab62a5f091f75
IrinaSkoryk <irinaskoryk36@gmail.com>
2025-02-21 22:36:24 +0100

    Update 0020-Task.sql

--- 0020-Task.sql
+++ 0020-Task.sql
@@ -3,15 +3,16 @@
 
 CREATE TABLE patients (  
   patient_id INT PRIMARY KEY
-  first_name TEXT  
-  last_name TEXT  
-  gender CHAR
-  birth_date   DATE  
-  city   TEXT  
-  FOREIGN KEY (province_id) REFERENCES province_names (province_id)
-  allergies TEXT  
-  height INT  
-  weight INT
+  , first_name TEXT  
+  , last_name TEXT  
+  , gender CHAR
+  , birth_date DATE  
+  , city TEXT  
+  , province_id INT
+  , allergies TEXT  
+  , height INT  
+  , weight INT  
+  , FOREIGN KEY (province_id) REFERENCES province_names (province_id)
 );
 
 UPDATE patients
