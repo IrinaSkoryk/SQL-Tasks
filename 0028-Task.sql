e51160c85ac7cee78d7f5ebc11bafa68e55e8b43
IrinaSkoryk <irinaskoryk36@gmail.com>
2025-02-21 22:27:46 +0100

    Update 0028-Task.sql

--- 0028-Task.sql
+++ 0028-Task.sql
@@ -2,15 +2,18 @@
 -- Show the patient id and the total number of admissions for patient_id 579.
 
 CREATE TABLE admissions (  
-  FOREIGN KEY (patient_id) REFERENCES patients (patient_id)  
-  admission_date DATE  
-  discharge_date   DATE  
-  diagnosis   TEXT
-  FOREIGN KEY (attending_doctor_id) REFERENCES doctors (doctor_id)
+  admission_id SERIAL PRIMARY KEY  
+  , patient_id INT
+  , admission_date DATE  
+  , discharge_date DATE  
+  , diagnosis TEXT  
+  , attending_doctor_id INT  
+  , FOREIGN KEY (patient_id) REFERENCES patients (patient_id)  
+  , FOREIGN KEY (attending_doctor_id) REFERENCES doctors (doctor_id)  
 );
 
 SELECT  
   patient_id  
-  ,COUNT(patient_id) AS total_admissions
+  , COUNT(patient_id) AS total_admissions
 FROM admissions
 WHERE patient_id = 579;
