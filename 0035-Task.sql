15747b844c00241c5d14b3693635bb5ddcee51bb
IrinaSkoryk <irinaskoryk36@gmail.com>
2025-02-22 13:52:03 +0100

    Update 0035-Task.sql

--- 0035-Task.sql
+++ 0035-Task.sql
@@ -1,4 +1,4 @@
--- #Medium
+-- #Medium #JOIN #WHERE
 -- Show patient_id, first_name, last_name from patients whos diagnosis is 'Dementia'. 
 -- Primary diagnosis is stored in the admissions table.
 
