ae91a02e5bb784006418a5f2e476b7a9b9c73f75
IrinaSkoryk <irinaskoryk36@gmail.com>
2025-02-22 11:52:13 +0100

    Create 0033-Task.sql

---
+++ 0033-Task.sql
@@ -0,0 +1,22 @@
+-- #Medium #GROUPBY #HAVING #COUNT #Calculations
+-- Show unique first names from the patients table which only occurs once in the list.
+-- For example, if two or more people are named 'John' in the first_name column then don't include their name in the output list. If only 1 person is named 'Leo' then include them in the output.
+
+CREATE TABLE patients (  
+  patient_id INT PRIMARY KEY
+  , first_name TEXT  
+  , last_name TEXT  
+  , gender CHAR
+  , birth_date DATE  
+  , city TEXT  
+  , province_id INT
+  , allergies TEXT  
+  , height INT  
+  , weight INT  
+  , FOREIGN KEY (province_id) REFERENCES province_names (province_id)
+);
+
+SELECT first_name
+FROM patients
+GROUP BY 1
+HAVING COUNT(first_name) = 1;
