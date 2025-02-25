54af6d05b818bb437804a12025448fcd81475c1a
IrinaSkoryk <irinaskoryk36@gmail.com>
2025-02-25 22:55:34 +0100

    Create 0044-Task.sql

---
+++ 0044-Task.sql
@@ -0,0 +1,22 @@
+-- #Medium #CONCAT #UPPER #LOWER #ORDERBY #
+/* We want to display each patient's full name in a single column. Their last_name in all upper letters must appear first, then first_name in all lower case letters. Separate the last_name and first_name with a comma. Order the list by the first_name in decending order
+EX: SMITH,jane */
+
+CREATE TABLE patients (  
+  patient_id INT PRIMARY KEY
+  , first_name TEXT  
+  , last_name TEXT  
+  , gender CHAR
+  , birth_date DATE  
+  , city TEXT  
+  , province_id INT
+  , allergies TEXT  
+  , height INT  
+  , weight INT  
+  , FOREIGN KEY (province_id) REFERENCES province_names (province_id)
+);
+
+SELECT 
+	CONCAT(UPPER(last_name), ',',LOWER(first_name)) AS new_name_format
+FROM patients
+ORDER BY first_name DESC;
